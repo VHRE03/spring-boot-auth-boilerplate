623c0b67294505325312e28046ebeaa471b5b211
VHRE03 <vh.romero.e@gmail.com>
2026-07-01 23:25:24 -0600

    first commit

---
+++ src/test/java/com/vhre/auth_boilerplate/AuthBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhre.auth_boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
